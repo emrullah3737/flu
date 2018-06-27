172bc1814551849dc91c38fe2f36fbcea22db54e
Emrullah Karatas <emrullah@efabrika.com>
2018-06-27 14:39:17 +0300

    login controller refactored

--- src/controller/login.js
+++ src/controller/login.js
@@ -3,15 +3,10 @@ import * as firebase from 'firebase';
 export default async (doc) => {
   const { email, password } = doc;
   try {
-    return await firebase.auth().signInWithEmailAndPassword(email, password);
+    await firebase.auth().signInWithEmailAndPassword(email, password);
   } catch (error) {
     return error;
   }
 
-  try {
-    const user = await firebase.auth().onAuthStateChanged();
-    return user;
-  } catch (error) {
-    return error;
-  }
+  return firebase.auth().currentUser;
 };
